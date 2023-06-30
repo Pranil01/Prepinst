72d4c7308138b903b6c08d81dd8882b8f956efba
Pranil01 <94604003+Pranil01@users.noreply.github.com>
2023-06-30 23:53:50 +0530

    Add files via upload

---
+++ RemoveAlpha1.java
@@ -0,0 +1,29 @@
+package RemoveExceptAlphabets;
+
+class RemoveAlpha1{
+
+    static void removeSpecialCharacter(String s)
+    {
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s.charAt(i) < 'A' || s.charAt(i) > 'Z' && s.charAt(i) < 'a' || s.charAt(i) > 'z')
+            {
+                s = s.substring(0, i) + s.substring(i + 1);
+                i--;
+            }
+          
+           
+        }  
+        System.out.print(s);
+
+       
+    }
+   
+    public static void main(String[] args)
+    {
+        String s = "@S*O%M$E#S)H==W=A7R";
+    
+        removeSpecialCharacter(s);
+       
+    }
+}
